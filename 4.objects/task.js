54471c4b68d7ff0c13af528b634326f998b0c998
Mary Chill <chilikina.mari@inbox.ru>
2023-02-05 19:49:47 +0300

    first

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,25 @@
 function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+  }
   
-}
-
-Student.prototype.setSubject = function (subjectName) {
+  Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
+  }
   
-}
-
-Student.prototype.addMarks = function (...marks) {
+  Student.prototype.addMarks = function (...marks) {
+      return this.marks ? this.marks.push(...marks) : 'студент отчислен';
+  }
   
-}
-
-Student.prototype.getAverage = function () {
+  Student.prototype.getAverage = function () {
+      return (!this.marks || !this.marks.length) ? 0 : this.marks.reduce(
+        (counter, value) => counter + value, 0) / this.marks.length;
+  }
   
-}
-
-Student.prototype.exclude = function (reason) {
-  
-}
+  Student.prototype.exclude = function (reason) {
+      delete this.subject;
+      delete this.marks;
+      this.exclude = reason;
+  }
